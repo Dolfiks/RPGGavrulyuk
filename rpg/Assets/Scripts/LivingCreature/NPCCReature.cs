ff881ae196d18813d1bbc325ec69a7599a806236
Dolfiks <46033744+Dolfiks@users.noreply.github.com>
2021-04-01 19:54:12 +0300

    lab1
    1

---
+++ rpg/Assets/Scripts/LivingCreature/NPCCReature.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCCReature : Interactable
+{
+    protected override void Interact()
+    {
+        base.Interact();
+        Debug.Log("Hi Bro, go to chat");
+    }
+}
